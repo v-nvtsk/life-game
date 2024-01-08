4f05053d6f4a17fc8b8aeb32f4827ae8d54e85b3
v-nvtsk <v@nvtsk.ru>
2024-01-08 20:12:52 +0300

    feat: add simple Life game

---
+++ src/count-neighbours.ts
@@ -0,0 +1,12 @@
+export default function countNeighbours(field: number[][], x: number, y: number): number {
+  let count = 0
+  for (let i = -1; i <= 1; i++) {
+    for (let j = -1; j <= 1; j++) {
+      if (x + i >= 0 && y + j >= 0 && x + i <= field.length - 1 && y + j <= field.length - 1) {
+        count += field[x + i][y + j]
+      }
+    }
+  }
+  count -= field[x][y]
+  return count
+}
