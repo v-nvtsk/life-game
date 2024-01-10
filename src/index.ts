f766ef4146291f8392395bcc5e70cf2715aec5fc
v-nvtsk <v@nvtsk.ru>
2024-01-10 17:09:49 +0300

    fix:  remove commented code

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,3 @@ document.body.append(container);
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const game = new Game(container);
 game.init();
-
-// const container1 = document.createElement("div");
-// container1.className = "game-container";
-// document.body.append(container1);
-// const game1 = new Game(container1);
-// game1.init();
