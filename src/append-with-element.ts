12c704987ca60d2bcb89b06e031f92a2c1b76097
v-nvtsk <v@nvtsk.ru>
2024-01-10 16:48:45 +0300

    refactor: remove append-with-element

--- src/append-with-element.ts
+++
@@ -1,9 +0,0 @@
-function appendWithElement(container: HTMLElement, tag: string, className = '', innerHTML = ''): HTMLElement {
-  const element = document.createElement(tag)
-  element.innerHTML = innerHTML
-  element.className = className
-  container.append(element)
-  return element
-}
-
-export default appendWithElement
